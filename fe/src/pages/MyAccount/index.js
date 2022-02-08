b9bcc6e2596b026e6bc963a898af19e7ab9a7e1c
chiupao <a0912979737@gmail.com>
2022-02-08 15:57:19 +0800

    style - add scss

--- fe/src/pages/MyAccount/index.js
+++ fe/src/pages/MyAccount/index.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { API_URL } from "../../utils/config";
+import "./myAccount.scss";
 
 const MyAccount = () => {
-  return <div></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // http://localhost:3002/api/users
+    let getUser = async () => {
+      let response = await axios.get(`${API_URL}/users`);
+      // response 是物件
+      setData(response.data);
+    };
+    getUser();
+  }, []);
+
+  return (
+    // <div>
+    //   {data.map((users) => {
+    //     return <div>{users.name}</div>;
+    //   })}
+    // </div>
+
+    <div>
+      {/* <div className="font">test 這樣成功嗎</div> */}
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-2">
+            <div className="leftMenu">
+              <div className="headShot"></div>
+              <p>王xx</p>
+            </div>
+          </div>
+          <div className="col-lg-10">456</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyAccount;
