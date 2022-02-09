001c5e84287c8503d76876b8a7e80d8713b7f586
Miru <hungche0701@gmail.com>
2022-02-09 17:21:08 +0800

    feat - 拆解左邊導覽列

---
+++ fe/src/pages/Storebg/component/LeftNavbar.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+// -------- icons 開始 --------
+// 比例圖 icon
+import { IoPieChartSharp } from "react-icons/io5";
+// -------- icons 結束 --------
+
+// Logo 圖片
+import { ReactComponent as Logo } from "../../../images/logo.svg";
+
+const LeftNavbar = () => {
+  return (
+    <>
+      <div className="navigation">
+        <ul>
+          <li>
+            <Logo />
+          </li>
+          <li>
+            <a href="#/">
+              <span className="icon">
+                <IoPieChartSharp />
+              </span>
+              <span className="title">商家資料編輯</span>
+            </a>
+          </li>
+          <li>
+            <a href="#/">
+              <span className="icon">
+                <IoPieChartSharp />
+              </span>
+              <span className="title">商品清單</span>
+            </a>
+          </li>
+          <li>
+            <a href="#/">
+              <span className="icon">
+                <IoPieChartSharp />
+              </span>
+              <span className="title">商品新增</span>
+            </a>
+          </li>
+          <li>
+            <a href="#/">
+              <span className="icon">
+                <IoPieChartSharp />
+              </span>
+              <span className="title">商品資料編輯</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default LeftNavbar;
