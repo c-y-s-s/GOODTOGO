008b6ec60f468da175dfdb65f43f48619a2a58ad
Your hadeli <ch96963@gmail.com>
2022-02-09 15:11:13 +0800

    refactor - product 元件新增

---
+++ fe/src/pages/Product/component/Storebutton.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Storebutton = () => {
+  return (
+    <div>
+      <div class="product-button text-center">
+        <button type="button" class="btn">
+          餐點
+        </button>
+        <button type="button" class="btn">
+          評論
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Storebutton;
