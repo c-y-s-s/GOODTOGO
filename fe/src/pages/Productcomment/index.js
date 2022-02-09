53b45f73ccc2c50647c2ae3a472fab6240b2e319
Chang <ch96963@gmail.com>
2022-02-09 23:14:33 +0800

    feat - 客戶評論區塊串接API成功撈到資料

--- fe/src/pages/Productcomment/index.js
+++ fe/src/pages/Productcomment/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import React from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../../utils/config";
+// import StoreLogo from "../Product/component/StoreLogo";
+// import StoreDetails from "../Product/component/StoreDetails";
+// import StoreCanopy from "../Product/component/StoreCanopy";
+// import Storebutton from "../Product/component/StoreButton";
+// import StoreCard from "../Product/component/StoreCard";
+// import Product from "../Product/index";
 
 const Productcomment = () => {
-  return <div></div>;
+  const { storeId } = useParams();
+  const [data, setData] = useState([]);
+  const [storeData, setStoreData] = useState([]);
+
+  useEffect(() => {
+    let getProducts = async () => {
+            let storeResponse = await axios.get(`${API_URL}/stores/${storeId}`);
+      let productsCommitResponse = await axios.get(
+        `${API_URL}/productscommit/${storeId}`
+      );
+      
+      setStoreData(storeResponse.data);
+      setData(productsCommitResponse.data);
+      
+      console.log(productsCommitResponse.data);
+    };
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 export default Productcomment;
