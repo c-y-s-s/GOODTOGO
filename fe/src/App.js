6ae24d0ab4125ce9f67f728dc2eb38de7462a407
ECHOSHIH <chunyushih@outlook.com>
2022-02-10 18:02:40 +0800

    fix - 恢復上一版本

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./styles/index.css";
+import "./styles/index.scss";
 
 // 這邊的資料夾命名方式可以不用指定裡面的 index
 import Navbar from "./components/Navbar";
@@ -17,22 +17,26 @@ import Register from "./pages/Register";
 import MyAccount from "./pages/MyAccount";
 import Product from "./pages/Product";
 import Footer from "./components/Footer";
-
+import ProductComment from "../src/pages/Productcomment" 
 function App() {
+  const [auth, setAuth] = useState(false);
   return (
     <Router>
-      <Navbar />
+      <Navbar auth={auth} />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/store" element={<Store />}></Route>
         {/* 店家商品頁，店家點進來顯示店家所賣商品 */}
         <Route path="/store/:storeId" element={<Product />}></Route>
+        <Route
+          path="/productcomment/:storeId"
+          element={<ProductComment />}
+        ></Route>
         <Route path="/map" element={<Map />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/my_account" element={<MyAccount />}></Route>
-
       </Routes>
 
       {/* <StoreList />
