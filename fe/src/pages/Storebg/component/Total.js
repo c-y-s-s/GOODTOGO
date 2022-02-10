a84f5b45adaebe538c1ec5799419acf596465d56
Miru <hungche0701@gmail.com>
2022-02-10 17:02:03 +0800

    feat - 修改總數

---
+++ fe/src/pages/Storebg/component/Total.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Total = () => {
+  return (
+    <>
+      <div className="store-bg-amount">
+        <div>
+          <h6>1~8筆</h6>
+        </div>
+        <div>
+          <h6>共20樣商品</h6>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Total;
