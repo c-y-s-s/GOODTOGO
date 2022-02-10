6eee57c21a8bd41444f30c5e999561e0579f7824
Miru <hungche0701@gmail.com>
2022-02-10 17:01:53 +0800

    feat - 修改至頂導覽列

--- fe/src/pages/Storebg/component/TopNavbar.js
+++ fe/src/pages/Storebg/component/TopNavbar.js
@@ -7,19 +7,22 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { BsBellFill } from "react-icons/bs";
 // -------- icons 結束 --------
 
-const TopNavbar = (name) => {
+const TopNavbar = ({ name }) => {
   return (
     <>
       <div className="topbar">
         <div className="product_list">商品清單</div>
-        <div className="search">
-          <input type="text" placeholder="Search here" />
-          <AiOutlineSearch />
-        </div>
-        <BsBellFill />
-        <div>小胖烘培屋-士林店</div>
-        <div className="user">
-          <img src={require("../images/img1.jpg")} alt="" />
+        <div className="sss">
+          <div className="search">
+            <input type="text" placeholder="Search here" />
+            <AiOutlineSearch />
+          </div>
+          <BsBellFill />
+          <div>{name}</div>
+          {/* <div>烘焙坊</div> */}
+          <div className="user">
+            <img src={require("../images/img1.jpg")} alt="" />
+          </div>
         </div>
       </div>
     </>
