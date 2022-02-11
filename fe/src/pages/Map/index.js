8fe8e9c3eab8997369141ae8d85c0a87e87239fd
Infiltrators-Elixir <bhst1124@gmail.com>
2022-02-11 16:46:41 +0800

    feat - 地圖與清單區塊調整&加入篩選區域

--- fe/src/pages/Map/index.js
+++ fe/src/pages/Map/index.js
@@ -1,79 +1,85 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import GoogleMapReact from "google-map-react";
+import { CircularProgress, Select, InputLabel, MenuItem, FormControl, CssBaseline, Grid, Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import './MapStyle.scss';
 
-// google map wrapper
-// import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
-//地圖CSS, 備用
-// import "./map.css";
-
-// remember to input the .env.GOOGLE_API_KEY
-
-//地圖圖標
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 25.0439355,
-      lng: 121.503584
-    },
-    zoom: 15
-  };
-  render() {
-    return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key:  }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={25.0439355}
-            lng={121.503584}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
-    );
-  }
-}
+// -------- Map功能開始 --------//
 
-//待處理 前端往後端請求 再由後端往map api打請求
+const Map = () => {
+  //-------- classes管理功能 --------//
+  // const classes = [];
+  //-------- classes管理功能 --------//
 
-// const Map: React.FC<{}> = () => {};
-// function Map() {
-//   return <>
-//     <head>
-//       <title>Simple Map</title>
-//       <meta name="viewport" content="initial-scale=1.0"/>
-//         <meta charset="utf-8"/>
-//         </head>
-//         <body>
-//           <div id="map"></div>
-//           <script>
-//             var map;
-//             function initMap() {
-//               map = new google.maps.Map(document.getElementById('map'), {
-//                 center: { lat: -34.397, lng: 150.644 },
-//                 zoom: 8
-//               })}
-//           </script>
-//           <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
-//             async defer></script>
-//         </body>
-//       </>
-// };
+  const isMobile = useMediaQuery('(min-width:300px)');
+  const coordinates = { lat: 0, lng: 0 };
+  const [opened, setOpened] = useState("opened");
+  const [type, setType] = useState("type1");
+  const [where, setWhere] = useState("chung-li");
 
 
-// App
-function Map() {
   return (
-    <div className="App">
-      <SimpleMap />
-    </div>
+    <>
+      <CssBaseline />
+      {/* -------- 地圖區塊開始 -------- */}
+      <Grid className="mapList" container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={9}>
+          <div className="mapContainer" >
+            <GoogleMapReact
+              //最後記得把KEY放進環境變數
+              bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+              defaultCenter={coordinates}
+              center={coordinates}
+              defaultZoom={13}
+              margin={[0, 0, 0, 0]}
+              options={""}
+              onChange={""}
+              onChildClick={""}
+            >
+            </GoogleMapReact>
+          </div>
+        </Grid>
+        {/* -------- 地圖區塊結束 -------- */}
+
+        {/* -------- 清單區塊開始 -------- */}
+        <Grid item xs={12} md={3}>
+          <div className="mapList">
+            <Typography variant="h4" align="center">
+              搜尋附近店家
+
+            </Typography>
+            <FormControl fullWidth className="formControl">
+              <Select className="selector" disableUnderline align="center" value={opened} onChange={(e) => setOpened(e.target.value)}>
+                <MenuItem value="opened">營業中店家</MenuItem>
+                <MenuItem value="closed">全部</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl fullWidth className="formControl">
+              <Select className="selector" disableUnderline align="center" value={type} onChange={(e) => setType(e.target.value)}>
+                <MenuItem value="type1">種類1</MenuItem>
+                <MenuItem value="type2">種類2</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl fullWidth className="formControl">
+              <Select className="selector" disableUnderline align="center" value={where} onChange={(e) => setWhere(e.target.value)}>
+                <MenuItem value="chung-li">中壢區</MenuItem>
+                <MenuItem value="ping-jun">平鎮區</MenuItem>
+              </Select>
+            </FormControl>
+            <Grid container spacing={3} className="list">
+
+              
+            </Grid>
+            </div>
+        </Grid>
+        {/* -------- 清單區塊結束 -------- */}
+      </Grid>
+    </>
   );
 }
 
-      export default Map;
+// --------Map元件導出-------- //
+export default Map;
