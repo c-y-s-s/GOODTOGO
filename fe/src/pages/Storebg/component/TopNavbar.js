429347a61107f8cd174f6273f5c9978c12a1c0bf
Miru <hungche0701@gmail.com>
2022-02-11 16:10:04 +0800

    feat - 改寫useEffect

--- fe/src/pages/Storebg/component/TopNavbar.js
+++ fe/src/pages/Storebg/component/TopNavbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 
 // -------- icons 開始 --------
 // 放大鏡 icon
@@ -7,18 +7,28 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { BsBellFill } from "react-icons/bs";
 // -------- icons 結束 --------
 
-const TopNavbar = ({ name }) => {
+const TopNavbar = (props) => {
+  const { storeData } = props;
+  const [storeName, setStoreName] = useState([])
+  useEffect(() => {
+    const findName = ()=>{
+      storeData.map((item, i)=>setStoreName([...storeName, item.name]))
+      console.log(storeName);
+    }
+    findName();
+  }, [])
+  
   return (
     <>
-      <div className="topbar">
-        <div className="product_list">商品清單</div>
-        <div className="sss">
+      <div className="topbar ">
+        <div className="product-list">商品清單</div>
+        <div className="storeBg-topNavbar-right">
           <div className="search">
             <input type="text" placeholder="Search here" />
-            <AiOutlineSearch />
+            <AiOutlineSearch className="storeBg-topNavbar-aiOutlineSearch me-2 my-2" />
           </div>
-          <BsBellFill />
-          <div>{name}</div>
+          <BsBellFill className="storeBg-topNavbar-bsBellFill" />
+          <div className="mx-3 border-start border-2 ps-3">烘焙坊</div>
           {/* <div>烘焙坊</div> */}
           <div className="user">
             <img src={require("../images/img1.jpg")} alt="" />
