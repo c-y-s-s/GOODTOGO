7d7f414f915da8343f142ecfd5c69a378018889c
ECHOSHIH <chunyushih@outlook.com>
2022-02-13 03:48:04 +0800

    feat - 會員註冊

--- fe/src/pages/Register/index.js
+++ fe/src/pages/Register/index.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ImFacebook2 } from "react-icons/im";
 import { ReactComponent as Logo } from "../../images/logo-face.svg";
-import TWzipcode from "react-twzipcode";
+import axios from "axios";
+import { API_URL } from "../../utils/config";
+import { ERR_MSG } from "../../utils/error";
 
 const Register = () => {
+  const [member, setMember] = useState({
+    email: "song@test.com",
+    password: "song12345",
+    confirmPassword: "song12345",
+    name: "song",
+    phone: "0911122233",
+  });
+
+  // -------- checkbox 同意條款 --------
+  const [agree, setAgree] = useState(true);
+  // -------- 處理表格改變 --------
+  const handleChange = (e) => {
+    setMember({ ...member, [e.target.name]: e.target.value });
+  };
+  // -------- 表單送出 --------
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      let response = await axios.post(`${API_URL}/auth/register`, member);
+      console.log(response.data);
+    } catch (e) {
+      // console.error("錯誤:", e.response.data);
+      console.error(ERR_MSG[e.response.data].code);
+    }
+  };
+
   return (
     <>
       <div className="container-fluid reg-con">
@@ -20,23 +49,29 @@ const Register = () => {
                   <div className="h6 ">會員註冊</div>
                   {/* -------- 表格開始 -------- */}
                   <div className="label-group d-flex text-start flex-column justify-content-evenly gy-2">
-                    <form className="d-flex flex-column ">
+                    <form
+                      className="d-flex flex-column"
+                      onSubmit={handleSubmit}
+                    >
                       {/* -------- 姓名 -------- */}
                       <label
-                        for=""
+                        htmlFor=""
                         className="col-form-label input-label-title text-green p-0"
                       >
                         姓名
                       </label>
                       <div class="form-floating mb-3">
                         <input
+                          name="name"
                           type="text"
                           className="form-control custom-input "
-                          id="floatingInput"
-                          placeholder="name@example.com"
+                          id="name"
+                          placeholder="name"
+                          value={member.name}
+                          onChange={handleChange}
                         />
                         <label
-                          for="floatingInput"
+                          htmlFor="name"
                           className="floating-label text-grey"
                         >
                           請填入中文 / 英文姓名
@@ -44,20 +79,23 @@ const Register = () => {
                       </div>
                       {/* -------- 電子郵件 -------- */}
                       <label
-                        for=""
-                        className="col-form-label input-label-title   text-green p-0"
+                        htmlFor=""
+                        className="col-form-label input-label-title text-green p-0"
                       >
                         電子郵件
                       </label>
                       <div class="form-floating mb-3">
                         <input
+                          name="email"
+                          value={member.email}
                           type="email"
-                          className="form-control custom-input "
-                          id="floatingInput"
-                          placeholder="name@example.com"
+                          className="form-control custom-input"
+                          id="email"
+                          placeholder="email"
+                          onChange={handleChange}
                         />
                         <label
-                          for="floatingInput"
+                          htmlFor="email"
                           className="floating-label  text-grey"
                         >
                           請填入電子信箱
@@ -72,82 +110,83 @@ const Register = () => {
                       </label>
                       <div class="form-floating mb-3">
                         <input
+                          name="password"
                           type="password"
                           className="form-control custom-input"
-                          id="floatingInput"
-                          placeholder="name@example.com"
+                          id="password"
+                          placeholder="密碼"
+                          value={member.password}
+                          onChange={handleChange}
                         />
                         <label
-                          for="floatingInput"
-                          className="floating-label  text-grey"
+                          htmlFor="password"
+                          className="floating-label text-grey"
                         >
                           請填入密碼
                         </label>
                       </div>
                       {/* -------- 密碼確認 -------- */}
                       <label
-                        for=""
-                        className="col-form-label input-label-title   text-green p-0"
+                        htmlFor=""
+                        className="col-form-label input-label-title text-green p-0"
                       >
                         密碼確認
                       </label>
                       <div class="form-floating mb-3">
                         <input
+                          name="confirmPassword"
                           type="password"
-                          className="form-control custom-input "
-                          id="floatingInput"
-                          placeholder="name@example.com"
+                          className="form-control custom-input"
+                          id="confirm-password"
+                          placeholder=""
+                          value={member.confirmPassword}
+                          onChange={handleChange}
                         />
                         <label
-                          for="floatingInput"
-                          className="floating-label  text-grey"
+                          htmlFor="confirm-password"
+                          className="floating-label text-grey"
                         >
                           請再次輸入密碼
                         </label>
                       </div>
                       {/* -------- 手機-------- */}
                       <label
-                        for=""
+                        htmlFor=""
                         className="col-form-label input-label-title  text-green p-0"
                       >
                         手機
                       </label>
                       <div class="form-floating mb-3">
                         <input
+                          name="phone"
                           type="phone"
                           className="form-control custom-input"
                           id="floatingInput"
                           placeholder="name@example.com"
+                          value={member.phone}
+                          maxLength="10"
+                          onChange={handleChange}
                         />
                         <label
-                          for="floatingInput"
+                          htmlFor="floatingInput"
                           className="floating-label  text-grey"
                         >
                           09XXXXXXXX
                         </label>
                       </div>
-                      {/* -------- 地址 -------- */}
-                      <label
-                        for=""
-                        className="col-form-label input-label-title  text-green p-0"
-                      >
-                        地址
-                      </label>
-                      <div class="col-lg-6 d-flex flex-column">
-                        <TWzipcode
-                          css={[
-                            " custom-input form-control county-sel mb-2",
-                            " custom-input form-control district-sel ",
-                            "form-control zipcode d-none",
-                          ]}
-
-                          // handleChangeCounty={this.handleChange}
-                          // handleChangeDistrict={this.handleChange}
-                          // handleChangeZipcode={this.handleChange}
+                      {/* -------- 使用者同意條款 -------- */}
+                      <div className="col-lg-12 align-items-center text-grey input-label-title ">
+                        <input
+                          type="checkbox"
+                          checked={agree}
+                          onChange={(e) => {
+                            setAgree(e.target.checked);
+                          }}
                         />
-                      </div>
-
-                      <div className="col-lg-6">
+                        我已閱讀並同意
+                        <Link to="" className="no-link text-yellow">
+                          用戶權益和隱私條款
+                        </Link>
                         {/* -------- 縣市 -------- */}
                         {/* <div class="col-lg-6">
                           <select
@@ -175,10 +214,11 @@ const Register = () => {
                           </select>
                         </div> */}
                       </div>
-
+                      {/* TODO:當同意條款為true時以下按鈕才生效 */}
                       <button
                         type="submit"
                         className="btn btn-register col-lg-12 mb-2"
+                        disabled={!agree}
                       >
                         註冊
                       </button>
@@ -188,7 +228,10 @@ const Register = () => {
                       <hr className="col-lg-5" />或<hr className="col-lg-5" />
                     </div>
                     {/* -------- Facebook 登入 -------- */}
-                    <button className="col-lg-12 btn-fb-login btn d-flex align-items-center text-center justify-content-center m-0 mb-3">
+                    <button
+                      className="col-lg-12 btn-fb-login btn d-flex align-items-center text-center justify-content-center m-0 mb-3"
+                      disabled={!agree}
+                    >
                       <ImFacebook2 className="big-icon col-lg-2" />
                       使用 Facebook 註冊<div className="col-lg-2"> </div>
                     </button>
