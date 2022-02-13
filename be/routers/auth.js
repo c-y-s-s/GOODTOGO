d19a37b7306b58fe66ed938dc035108b913ae801
ECHOSHIH <chunyushih@outlook.com>
2022-02-13 23:31:17 +0800

    doc - app.js拿掉拿掉root overflow:hidden; be在在server.js加上加上urlencode

--- be/routers/auth.js
+++ be/routers/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const argon = require("argon2");
 const connection = require("../utils/db");
 
 //-------- 後端驗證套件 express-validator --------
@@ -11,14 +12,14 @@ const passwordRule = [
   body("confirmPassword")
     .trim()
     .custom((value, { req }) => {
-      return value === req.body.password;
+      return value == req.body.password;
     })
     .withMessage("兩次輸入的密碼不相同"),
 ];
 
 // /api/auth/register
 router.post("/register", emailRule, passwordRule, async (req, res, next) => {
-  // console.log(res.body);
+  console.log(req.body);
   //TODO: 確認格式是否正確
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -27,10 +28,19 @@ router.post("/register", emailRule, passwordRule, async (req, res, next) => {
     // return res.status(400).json({ code: "33001", msg: error[0].msg });
     return res.status(400).json({ errors: error });
   }
-  //TODO:檢查 email 是不是已經註冊
-  let users = await connection.execute("SELECT * FROM users WHERE email=?", [
-    req.body.email,
-  ]);
+  // //TODO:檢查 email 是不是已經註冊
+  let [members] = await connection.execute(
+    "SELECT * FROM users WHERE email=?",
+    [req.body.email]
+  );
+  console.log(members);
+  if (members.length > 0) {
+    return res.status(400).send({
+      code: "33002",
+      msg: "這個 email 已經已經註冊過了",
+    });
+  }
+
   // console.log(users);
   //TODO:雜湊密碼
   //TODO:存入資料庫
