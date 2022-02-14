315c98710b1513badf19c1a5f2684b33e13466bc
ECHOSHIH <chunyushih@outlook.com>
2022-02-14 14:18:28 +0800

    doc - 解衝突

--- fe/src/pages/Login/index.js
+++ fe/src/pages/Login/index.js
@@ -13,9 +13,62 @@ const Login = () => {
           <div className="col-lg-4 m-0 p-0">
             <div className="content text-center row justify-content-between gy-0">
               <div className="col-lg-1"></div>
+<<<<<<< HEAD
               <div className="col-lg-10 row mt-4 mb-4 flex-column justify-content-evenly align-items-center">
                 <LoginForm />
                 {/* <Reset /> */}
+=======
+              <div className="col-lg-10">
+                <div className="h3">會員登入</div>
+                <form>
+                  <div className="label-group">
+                    {/* email */}
+                    <label for="" className="col-form-label label-title p-0">
+                      電子郵件
+                    </label>
+                    <div class="form-floating mb-3">
+                      <input
+                        type="email"
+                        className="form-control "
+                        id="floatingInput"
+                        placeholder="name@example.com"
+                      />
+                      <label for="floatingInput" className="floating-label">
+                        請填入電子信箱
+                      </label>
+                    </div>
+                    {/* password */}
+                    <label for="" className="col-form-label label-title p-0">
+                      密碼
+                    </label>
+                    <div class="form-floating mb-3">
+                      <input
+                        type="password"
+                        className="form-control "
+                        id="floatingInput"
+                        placeholder="name@example.com"
+                      />
+                      <label for="floatingInput" className="floating-label">
+                        請填入密碼
+                      </label>
+                    </div>
+                  </div>
+                  <button type="submit" className="submit-btn mb-3">
+                    登入
+                  </button>
+                </form>
+                <button className="fb-login">使用Facebook登入</button>
+                <Link to="">
+                  <p>忘記密碼？</p>
+                </Link>
+
+                <p>
+                  尚未加入GOODTOGO？
+                  <Link to="/register">
+                    <span>立即註冊</span>
+                  </Link>
+                </p>
+>>>>>>> 53c5f90 (feat - login)
               </div>
               <div className="col-lg-1"></div>
             </div>
