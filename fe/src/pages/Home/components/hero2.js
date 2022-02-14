e188eecce873211615003d08e171a9e996926ce6
ECHOSHIH <chunyushih@outlook.com>
2022-02-14 14:44:47 +0800

    doc - rebase

--- fe/src/pages/Home/components/hero2.js
+++ fe/src/pages/Home/components/hero2.js
@@ -2,21 +2,13 @@ import React from "react";
 
 // -------- svg圖檔 --------
 import { ReactComponent as Hero2Text } from "../images/hero2Text.svg";
-<<<<<<< HEAD
 // import { ReactComponent as Hero2Trash } from "../images/hero2Trash.svg";
-=======
-import { ReactComponent as Hero2Trash } from "../images/hero2Trash.svg";
->>>>>>> 9520249 (doc - 將首頁模組化)
 
 const hero2 = () => {
   return (
     <div className="hero2 d-flex">
       <div className="col-lg-6">
-<<<<<<< HEAD
         <img src={require("../images/hero2Trash.png")} alt="hero2Trash" />
-=======
-        <Hero2Trash />
->>>>>>> 9520249 (doc - 將首頁模組化)
       </div>
       <div className="col-lg-6">
         <div className="hero2Info hero-text-sm">
