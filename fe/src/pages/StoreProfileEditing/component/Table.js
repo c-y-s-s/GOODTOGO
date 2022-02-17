5fed2632f17c89af783d5c26b6f6155f0c20c905
Miru <hungche0701@gmail.com>
2022-02-18 01:15:43 +0800

    feat - 修改送出申請按鈕位置

--- fe/src/pages/StoreProfileEditing/component/Table.js
+++ fe/src/pages/StoreProfileEditing/component/Table.js
@@ -3,7 +3,7 @@ const Table = () => {
   return (
     <div>
       <div className="container">
-        <div className="row">
+        <div className="row ">
           <div className="col-6">
             <div className="mb-3">
               <label htmlFor="name" className="form-label fw-bold">
@@ -121,185 +121,187 @@ const Table = () => {
               </div>
             </div>
           </div>
-          <div className="col-6 d-flex flex-column">
-            <div className="mb-3">
-              <div className="d-flex justify-content-between">
-                <label htmlFor="logoImage" className="form-label fw-bold">
-                  店家 Logo 圖像
-                </label>
-                <label className="text-danger">
-                  限用.jpg/.jpeg/.png檔 上限 2MB
-                </label>
-              </div>
-              <input
-                className=" form-control"
-                type="file"
-                id="logoImage"
-                accept=".png, .jpg, .jpeg"
-              />
-            </div>
-            <div className="mb-3">
-              <div className="d-flex justify-content-between">
-                <label htmlFor="logoImage" className="form-label fw-bold">
-                  店家營業登記證
-                </label>
-                <label className="text-danger">
-                  限用.jpg/.jpeg/.png檔 上限 2MB
-                </label>
-              </div>
-              <input
-                className=" form-control"
-                type="file"
-                id="businessLicense"
-                accept=".png, .jpg, .jpeg"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="name" className="form-label fw-bold">
-                營業星期 (複選)
-              </label>
-              <div className="businessWeek-bg p-3">
-                <div className="d-flex justify-content-around">
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="mon"
-                      value="1"
-                    />
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="tue"
-                      value="2"
-                    />
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="wed"
-                      value="3"
-                    />
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="thu"
-                      value="4"
-                    />
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="fri"
-                      value="5"
-                    />
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="sat"
-                      value="6"
-                    />
-                  </div>
-                  <div className="form-check form-check-inline">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="sun"
-                      value="0"
-                    />
-                  </div>
-                </div>
-                <div className="d-flex justify-content-around  mt-2 businessWeek-align">
-                  <label
-                    className="form-check-label  form-check-inline text-center"
-                    htmlFor="mon"
-                  >
-                    一
+          <div className="col-6 d-flex justify-content-between  flex-column">
+            <div>
+              <div className="mb-3">
+                <div className="d-flex justify-content-between">
+                  <label htmlFor="logoImage" className="form-label fw-bold">
+                    店家 Logo 圖像
                   </label>
-                  <label
-                    className="form-check-label  form-check-inline"
-                    htmlFor="tue"
-                  >
-                    二
-                  </label>
-                  <label
-                    className="form-check-label  form-check-inline"
-                    htmlFor="wed"
-                  >
-                    三
-                  </label>
-                  <label
-                    className="form-check-label  form-check-inline"
-                    htmlFor="thu"
-                  >
-                    四
-                  </label>
-                  <label
-                    className="form-check-label  form-check-inline"
-                    htmlFor="fri"
-                  >
-                    五
-                  </label>
-                  <label
-                    className="form-check-label  form-check-inline"
-                    htmlFor="sat"
-                  >
-                    六
-                  </label>
-                  <label
-                    className="form-check-label  form-check-inline"
-                    htmlFor="sun"
-                  >
-                    日
+                  <label className="text-danger">
+                    限用.jpg/.jpeg/.png檔 上限 2MB
                   </label>
                 </div>
+                <input
+                  className=" form-control"
+                  type="file"
+                  id="logoImage"
+                  accept=".png, .jpg, .jpeg"
+                />
+              </div>
+              <div className="mb-3">
+                <div className="d-flex justify-content-between">
+                  <label htmlFor="logoImage" className="form-label fw-bold">
+                    店家營業登記證
+                  </label>
+                  <label className="text-danger">
+                    限用.jpg/.jpeg/.png檔 上限 2MB
+                  </label>
+                </div>
+                <input
+                  className=" form-control"
+                  type="file"
+                  id="businessLicense"
+                  accept=".png, .jpg, .jpeg"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label fw-bold">
+                  營業星期 (複選)
+                </label>
+                <div className="businessWeek-bg p-3">
+                  <div className="d-flex justify-content-around">
+                    <div className="form-check form-check-inline">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="mon"
+                        value="1"
+                      />
+                    </div>
+                    <div className="form-check form-check-inline">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="tue"
+                        value="2"
+                      />
+                    </div>
+                    <div className="form-check form-check-inline">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="wed"
+                        value="3"
+                      />
+                    </div>
+                    <div className="form-check form-check-inline">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="thu"
+                        value="4"
+                      />
+                    </div>
+                    <div className="form-check form-check-inline">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="fri"
+                        value="5"
+                      />
+                    </div>
+                    <div className="form-check form-check-inline">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="sat"
+                        value="6"
+                      />
+                    </div>
+                    <div className="form-check form-check-inline">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="sun"
+                        value="0"
+                      />
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-around  mt-2 businessWeek-align">
+                    <label
+                      className="form-check-label  form-check-inline text-center"
+                      htmlFor="mon"
+                    >
+                      一
+                    </label>
+                    <label
+                      className="form-check-label  form-check-inline"
+                      htmlFor="tue"
+                    >
+                      二
+                    </label>
+                    <label
+                      className="form-check-label  form-check-inline"
+                      htmlFor="wed"
+                    >
+                      三
+                    </label>
+                    <label
+                      className="form-check-label  form-check-inline"
+                      htmlFor="thu"
+                    >
+                      四
+                    </label>
+                    <label
+                      className="form-check-label  form-check-inline"
+                      htmlFor="fri"
+                    >
+                      五
+                    </label>
+                    <label
+                      className="form-check-label  form-check-inline"
+                      htmlFor="sat"
+                    >
+                      六
+                    </label>
+                    <label
+                      className="form-check-label  form-check-inline"
+                      htmlFor="sun"
+                    >
+                      日
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="productCategory" className="form-label fw-bold">
+                  店家類別
+                </label>
+                <select
+                  className="form-select"
+                  id="productCategory"
+                  aria-label="Example select with button addon"
+                  defaultValue=""
+                >
+                  <option value="">請選擇店家販售商品類別</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div className="row align-items-center">
+                <label htmlFor="SalesTime" className="form-label fw-bold">
+                  營業時間（24小時制）
+                </label>
+                <div className="col">
+                  <input className="form-control" id="SalesTime" required />
+                </div>
+                ：
+                <div className="col">
+                  <input className="form-control" id="SalesTime" required />
+                </div>
+                ～
+                <div className="col">
+                  <input className="form-control" id="SalesTime" required />
+                </div>
+                ：
+                <div className="col">
+                  <input className="form-control" id="SalesTime" required />
+                </div>
               </div>
             </div>
-            <div className="mb-3">
-              <label htmlFor="productCategory" className="form-label fw-bold">
-                店家類別
-              </label>
-              <select
-                className="form-select"
-                id="productCategory"
-                aria-label="Example select with button addon"
-                defaultValue=""
-              >
-                <option value="">請選擇店家販售商品類別</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-            <div className="row align-items-center">
-              <label htmlFor="SalesTime" className="form-label fw-bold">
-                營業時間（24小時制）
-              </label>
-              <div className="col">
-                <input className="form-control" id="SalesTime" required />
-              </div>
-              ：
-              <div className="col">
-                <input className="form-control" id="SalesTime" required />
-              </div>
-              ～
-              <div className="col">
-                <input className="form-control" id="SalesTime" required />
-              </div>
-              ：
-              <div className="col">
-                <input className="form-control" id="SalesTime" required />
-              </div>
-            </div>
-            <div className="row mt-3 px-3 ">
+            <div className="row px-3 pb-3">
               <button type="submit" className="btn sendApply text-light ">
                 送 出 申 請
               </button>
