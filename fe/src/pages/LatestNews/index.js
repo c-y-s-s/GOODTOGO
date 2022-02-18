645ba35f93b27156cb09300b6f5635696a234aa8
Miru <hungche0701@gmail.com>
2022-02-18 18:15:20 +0800

    feat - 模組化修改

--- fe/src/pages/LatestNews/index.js
+++ fe/src/pages/LatestNews/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Breadcrumb from "./component/Breadcrumb";
 import Headings from "./component/Headings";
+import Card from "./component/Card";
+import Pagination from "./component/Pagination";
 
 const LatestNews = () => {
   return (
@@ -8,26 +10,8 @@ const LatestNews = () => {
       <div className="container">
         <Breadcrumb />
         <Headings />
-        <div className="card mb-3">
-          <div className="row g-0">
-            <div className="col-md-4">
-              <img src="..." className="img-fluid rounded-start" alt="..." />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <p className="card-text">
-                  <small className="text-muted">Last updated 3 mins ago</small>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Card />
+        <Pagination />
       </div>
     </div>
   );
