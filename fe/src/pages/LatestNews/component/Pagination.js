b3d174fab49a594050f3b0524126259f21c4e0a6
Miru <hungche0701@gmail.com>
2022-02-18 18:18:52 +0800

    feat - 最新消息分頁

---
+++ fe/src/pages/LatestNews/component/Pagination.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Pagination = () => {
+  return (
+    <div>
+      <nav
+        aria-label="Page navigation example m-auto "
+        className="background-storebg-data-right-content-pages"
+      >
+        <ul className="pagination justify-content-center">
+          <li className="page-item">
+            <a className="page-link" href="#/" aria-label="Previous">
+              <span aria-hidden="true">&lt;</span>
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#/">
+              1
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#/">
+              2
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#/">
+              3
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#/" aria-label="Next">
+              <span aria-hidden="true">&gt;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Pagination;
