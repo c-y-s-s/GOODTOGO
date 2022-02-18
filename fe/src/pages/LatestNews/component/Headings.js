960cb08fa1dfb98068c71f675b1515444714c896
Miru <hungche0701@gmail.com>
2022-02-18 12:42:41 +0800

    feat - 最新消息標題

---
+++ fe/src/pages/LatestNews/component/Headings.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Headings = () => {
+  return (
+    <div>
+      <div className="d-flex justify-content-center">
+        <u>
+          <h3>最新消息</h3>
+        </u>
+      </div>
+    </div>
+  );
+};
+
+export default Headings;
