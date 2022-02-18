02fb2bdd6eebb77cad3f826e65a1de8d22d17c3a
Miru <hungche0701@gmail.com>
2022-02-18 18:16:28 +0800

    feat - 最新消息標題

--- fe/src/pages/LatestNews/component/Headings.js
+++ fe/src/pages/LatestNews/component/Headings.js
@@ -3,10 +3,10 @@ import React from "react";
 const Headings = () => {
   return (
     <div>
-      <div className="d-flex justify-content-center">
-        <u>
+      <div className="d-flex justify-content-center mb-5">
+        <div className="headings-bottom-line">
           <h3>最新消息</h3>
-        </u>
+        </div>
       </div>
     </div>
   );
