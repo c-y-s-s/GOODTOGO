1bf2cbc758e2802c8772f6fca2370cd39962e953
Miru <hungche0701@gmail.com>
2022-02-18 18:12:39 +0800

    feat - 最新活動標題

---
+++ fe/src/pages/Activity/component/Headings.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Headings = () => {
+  return (
+    <div>
+      <div className="d-flex justify-content-center mb-5">
+        <div>
+          <h3>【優惠活動】慶祝迎接新年，最新優惠活動</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Headings;
