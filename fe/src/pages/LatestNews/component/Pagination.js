2e897f17ce846c98eba9d92d4cff08f45f360b2e
Miru <hungche0701@gmail.com>
2022-02-21 01:40:14 +0800

    feat - 修改頁數樣式

--- fe/src/pages/LatestNews/component/Pagination.js
+++ fe/src/pages/LatestNews/component/Pagination.js
@@ -5,31 +5,39 @@ const Pagination = () => {
     <div>
       <nav
         aria-label="Page navigation example m-auto "
-        className="background-storebg-data-right-content-pages"
+        className="background-latestnews-data-right-content-pages"
       >
         <ul className="pagination justify-content-center">
           <li className="page-item">
-            <a className="page-link" href="#/" aria-label="Previous">
-              <span aria-hidden="true">&lt;</span>
+            <a
+              className="page-link rounded-circle border-0"
+              href="#/"
+              aria-label="Previous"
+            >
+              <span aria-hidden="true" className="aaa">&lt;</span>
             </a>
           </li>
           <li className="page-item">
-            <a className="page-link" href="#/">
+            <a className="page-link rounded-circle border-0" href="#/">
               1
             </a>
           </li>
           <li className="page-item">
-            <a className="page-link" href="#/">
+            <a className="page-link rounded-circle border-0" href="#/">
               2
             </a>
           </li>
           <li className="page-item">
-            <a className="page-link" href="#/">
+            <a className="page-link rounded-circle border-0" href="#/">
               3
             </a>
           </li>
           <li className="page-item">
-            <a className="page-link" href="#/" aria-label="Next">
+            <a
+              className="page-link rounded-circle border-0"
+              href="#/"
+              aria-label="Next"
+            >
               <span aria-hidden="true">&gt;</span>
             </a>
           </li>
