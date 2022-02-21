6a4d3e7ba285f9a7ffa7bd4c4e2d913c0dc0c5c0
Miru <hungche0701@gmail.com>
2022-02-21 14:38:36 +0800

    feat - 優惠劵頁面麵包屑

---
+++ fe/src/pages/Coupon/component/Breadcrumb.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Breadcrumb = () => {
+  return (
+    <div>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb mt-5">
+          <li className="breadcrumb-item ">
+            <a href="#/" className="text-decoration-none">
+              Home
+            </a>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            優惠卷
+          </li>
+        </ol>
+      </nav>
+    </div>
+  );
+};
+
+export default Breadcrumb;
