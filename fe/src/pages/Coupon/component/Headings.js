aa8a87f0985121ec1bfb7259db08bd250c8410fb
Miru <hungche0701@gmail.com>
2022-02-21 14:39:18 +0800

    feat - 優惠劵標題

---
+++ fe/src/pages/Coupon/component/Headings.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Headings = () => {
+  return (
+    <div>
+      <div className="d-flex justify-content-center mb-5">
+        <div className="headings-bottom-line">
+          <h3>
+            <img
+              src={require(`../images/Vector.png`)}
+              alt="..."
+              className="me-2 mb-2"
+            />
+            優惠卷
+            <img
+              src={require(`../images/Vector.png`)}
+              alt="..."
+              className="ms-2 mb-2"
+            />
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Headings;
