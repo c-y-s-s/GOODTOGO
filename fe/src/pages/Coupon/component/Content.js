3628b8e967aa571770519bf8d5019a6f6f92f9b0
Miru <hungche0701@gmail.com>
2022-02-22 02:42:30 +0800

    feat - 增加優惠劵內容

--- fe/src/pages/Coupon/component/Content.js
+++ fe/src/pages/Coupon/component/Content.js
@@ -3,28 +3,68 @@ import React from "react";
 const Card = () => {
   return (
     <div>
-      <div className="row">
-        <div className="col-4">
+      <div className="row py-5 px-3 row-390">
+        <div className="col">
+          <div className="time-title">2022-02-01</div>
           <img
             src={require(`../images/coupon5.png`)}
             alt="..."
-            className="card-images"
+            className="coupon-images"
           />
+          {/* <div className="time-title d-flex justify-content-center aaa">
+            剩餘數量：99
+          </div> */}
+          <h6 class="number-title">
+            <span> 剩餘數量：99</span>
+          </h6>
         </div>
-        <div className="col-4">
-        <img
-            src={require(`../images/coupon5.png`)}
+        <div className="col">
+          <div className="time-title">2022-02-01</div>
+          <img
+            src={require(`../images/coupon20.png`)}
             alt="..."
-            className="card-images"
+            className="coupon-images"
           />
+          <h6 class="number-title">
+            <span> 剩餘數量：99</span>
+          </h6>
         </div>
-        <div className="col-4">
-        <img
-            src={require(`../images/coupon5.png`)}
+        <div className="col">
+          <div className="time-title">2022-02-01</div>
+          <img
+            src={require(`../images/coupon30.png`)}
             alt="..."
-            className="card-images"
+            className="coupon-images"
           />
+          <h6 class="number-title">
+            <span> 剩餘數量：99</span>
+          </h6>
         </div>
+      </div>
+      <div className="row py-5 px-3 row-390">
+        <div className="col">
+          <div className="time-title">2022-02-01</div>
+          <img
+            src={require(`../images/coupon40.png`)}
+            alt="..."
+            className="coupon-images"
+          />
+          <h6 class="number-title">
+            <span> 剩餘數量：99</span>
+          </h6>
+        </div>
+        <div className="col">
+          <div className="time-title">2022-02-01</div>
+          <img
+            src={require(`../images/coupon60.png`)}
+            alt="..."
+            className="coupon-images"
+          />
+          <h6 class="number-title">
+            <span> 剩餘數量：99</span>
+          </h6>
+        </div>
+        <div className="col"></div>
       </div>
     </div>
   );
