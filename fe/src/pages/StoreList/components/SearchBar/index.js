00259e2fc08705424a0f9f62962d8a4e0e34f755
ECHOSHIH <chunyushih@outlook.com>
2022-02-26 14:26:33 +0800

    doc - searchBar測試

--- fe/src/pages/StoreList/components/SearchBar/index.js
+++ fe/src/pages/StoreList/components/SearchBar/index.js
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
+//後端套件
+import axios from "axios";
+import { API_URL } from "../../../../utils/config";
 
 const SearchBar = (props) => {
   const { setSearchOn } = props;
+  const [searchname, setSearchName] = useState();
 
+  useEffect(() => {
+    let getRecomm = async () => {
+      let recommRes = await axios.get(`${API_URL}/storeRecommRouter`);
+      let recommStores = recommRes.data[0];
+    };
+    getRecomm();
+  }, []);
   return (
     <div className="search-box col-lg-3 col-12 col-md-4 col-sm-5">
-      <form method="GET">
-        <HiOutlineSearch type="submit" className="ms-2" />
-        <input
-          name="keyword"
-          type="text"
-          className="store-search"
-          placeholder="熱門搜尋: 麵包"
-          onClick={(e) => {
-            e.preventDefault();
-            setSearchOn(true);
-          }}
-        />
-      </form>
+      <HiOutlineSearch type="submit" className="ms-2" />
+
+      <input
+        name="keyword"
+        type="text"
+        className="store-search"
+        placeholder="熱門搜尋: 麵包"
+        onChange={handleOnChange}
+      />
     </div>
   );
 };
