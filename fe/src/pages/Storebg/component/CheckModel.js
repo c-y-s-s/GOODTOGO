10877cfc45b73ec1a89090ef65a1c640108c9226
Miru <hungche0701@gmail.com>
2022-03-01 09:53:08 +0800

    feat - 修改model css 判斷

--- fe/src/pages/Storebg/component/CheckModel.js
+++ fe/src/pages/Storebg/component/CheckModel.js
@@ -1,38 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { API_URL } from "../../../utils/config";
+import { ERR_MSG } from "../../../utils/error";
 
-const CheckModel = ({productValid, productId}) => {
-    console.log("bbbbbbbbbbb",parseInt(productValid))
+const CheckModel = ({ productValid, productId }) => {
+  // 上下架 修改按鈕
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      // http://localhost:3002/api/member/password (router.post)
+      let response = await axios.post(`${API_URL}/storebg/productslistvalid`);
+      console.log("會員有更改密碼: ", response.data);
+    } catch (e) {
+      console.error("valid error: ", ERR_MSG[e.response.data.code]);
+      console.error("res.error:", e.response.data);
+      // setErr(e.response.data.msg);
+      // setErr({ ...err, confirmPassword: e.response.data.msg });
+    }
+  }
   return (
-    <div>               <div className="modal-dialog modal-sm modal-dialog-centered">
-    <div className="modal-content">
-      <div className="modal-header">
-        <button
-          type="button"
-          className="btn-close"
-          data-bs-dismiss="modal"
-          aria-label="Close"
-        ></button>
-      </div>
-      <div className="modal-body mx-auto fw-bold">
-      {/* 不管item.valid 等於 1 或 0 只會顯示 true 的結果 */}
-        {productValid === 1 ? "確定下架商品嗎?" : "確定上架商品嗎?"}
-      </div>   
-      <div className="modal-footer  mx-auto">
-        {/* <button
-          type="button"
-          className="btn btn-secondary"
-          data-bs-dismiss="modal"
-        >
-          Close
-        </button> */}
-     
-        <button type="button" className="btn btn-danger">
-          確定 {productValid} - {productId}
-        </button>
+    <div>
+      <div
+        className="modal fade"
+        id={"takeDown" + productId}
+        // tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-sm modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-footer storebg-modal-header">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body mx-auto fw-bold">
+              {/* 不管item.valid 等於 1 或 0 只會顯示 true 的結果 */}
+              {productValid === 1 ? "確定下架商品嗎?" : "確定上架商品嗎?"}
+            </div>
+            <div className="modal-footer storebg-modal-footer  mx-auto">
+              <button
+                type="submit"
+                className={
+                  (productValid === 1
+                    ? "btn-danger"
+                    : "storebg-model-btn-green") + " btn"
+                }
+                onClick={handleSubmit}
+              >
+                確定
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div></div>
-  )
-}
+  );
+};
 
-export default CheckModel
+export default CheckModel;
