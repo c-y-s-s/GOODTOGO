2989e2b499352d858ca8a042fc508577b25c43d1
Miru <hungche0701@gmail.com>
2022-03-01 18:43:57 +0800

    feat - 新增新增商品連結

--- fe/src/pages/Storebg/component/FilterContent.js
+++ fe/src/pages/Storebg/component/FilterContent.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
 import { FiFilter } from "react-icons/fi";
 const Filtercontent = () => {
   return (
@@ -13,9 +15,13 @@ const Filtercontent = () => {
           <FiFilter />
           排序
         </button>
-        <button type="button" className="btn btn-warning mx-3 text-white rounded-pill">
-            新增商品
-          </button>
+        <NavLink
+          type="button"
+          className="btn btn-warning mx-3 text-white rounded-pill"
+          to="/newproduct"
+        >
+          新增商品
+        </NavLink>
         <ul className="dropdown-menu background-storebg-data-right-sort-options">
           <li>
             <a href="#/">價格高到低</a>
