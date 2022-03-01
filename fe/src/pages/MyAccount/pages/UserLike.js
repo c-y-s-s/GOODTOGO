787656cb6bf0ace2525bc434a926b61981e52a42
chiupao <a0912979737@gmail.com>
2022-03-01 23:55:35 +0800

    feat - 商家卡片載入loading畫面、店家類別選單顯示ok

--- fe/src/pages/MyAccount/pages/UserLike.js
+++ fe/src/pages/MyAccount/pages/UserLike.js
@@ -5,8 +5,23 @@ import { API_URL, IMAGE_URL, ERR_MSG } from "../../../utils/config";
 import { FiClock, FiHeart, FiChevronDown } from "react-icons/fi";
 // import storeImage from "../../../images/store_img/22.jpg";
 import { Rating } from "@mui/material";
+import { css } from "@emotion/react";
+import BarLoader from "react-spinners/BarLoader";
 
 const UserLike = () => {
+  // loading 開關
+  const [loading, setLoading] = useState(false);
+  const [color, setColor] = useState("#A6BF4B");
+  const override = css`
+    display: block;
+    margin: 100px auto;
+  `;
+  // const spinner = (
+  //   <div className="spinner-border text-info" role="status">
+  //     <span className="sr-only"></span>
+  //   </div>
+  // );
+
   // 儲存 喜愛店家清單
   const [likeStores, setLikeStores] = useState([]);
 
@@ -21,13 +36,17 @@ const UserLike = () => {
   const [storeCategory, setStoreCategory] = useState([]);
 
   // 顯示 喜愛店家清單 各類別
-  const [showStoreCategory, setShowStoreCategory] = useState([]);
+  // const [showStoreCategory, setShowStoreCategory] = useState([]);
 
-  // // 紀錄選單目前顯示類別
-  // const [showCate, setShowCate] = useState("店家類別");
+  // 紀錄選單目前顯示類別
+  const [showCate, setShowCate] = useState("全部店家");
+  // console.log(showCate);
 
   // 載入 使用者收藏店家清單
   useEffect(() => {
+    // loading 開
+    setLoading(true);
+
     // http://localhost:3002/api/member/like
     let getLike = async () => {
       let response = await axios.get(`${API_URL}/member/like`, {
@@ -45,18 +64,23 @@ const UserLike = () => {
         "api/member/like(get) response.data.likeStoreIds: ",
         response.data.likeStoreIds
       );
-      console.log(
-        "api/member/like(get) response.data.userLikeStores.storeCate: ",
-        response.data.userLikeStores.map((v) => v.storeCate)
-      );
+      // console.log(
+      //   "api/member/like(get) response.data.userLikeStores.storeCate: ",
+      //   response.data.userLikeStores.map((v) => v.storeCate)
+      // );
 
       setLikeStores(response.data.userLikeStores); // 儲存 喜愛店家清單
       setShowLikeStores(response.data.userLikeStores); // 顯示 喜愛店家清單
       setLikeStoreIds(response.data.likeStoreIds); // 喜愛店家 store_id 選單
       setStoreCategory(response.data.storeCategories); // 店家類別 列表
-      setShowStoreCategory(
-        response.data.userLikeStores.map((v) => v.storeCate)
-      ); // 顯示 喜愛店家清單 類別
+      // setShowStoreCategory(
+      //   response.data.userLikeStores.map((v) => v.storeCate)
+      // ); // 顯示 喜愛店家清單 各類別
+
+      // loading 關
+      setTimeout(() => {
+        setLoading(false);
+      }, 1500);
 
       // test 資料濾除
       // console.log([...likeStoreIds].filter((v) => v !==5));
@@ -116,7 +140,10 @@ const UserLike = () => {
           </div>
           <div className="page_Title_Category_Hover">
             <div className="page_Title_Category d-flex justify-content-center justify-content-md-start align-items-center">
-              店家類別
+              <span className="page_Title_Text">
+                {showCate!=="全部店家" ? showCate+"店家" : "全部店家"}
+              </span>
+              {/* <span className="page_Title_Text">&nbsp;&nbsp;店&nbsp;家</span> */}
               <FiChevronDown className="ms-1 page_Title_Arrow" />
             </div>
             <ul className="page_Title_Category_List list-unstyled position-absolute mt-4">
@@ -129,13 +156,17 @@ const UserLike = () => {
                       // 篩選出 選取的類別 店家清單
                       // 符合條件留下 (比對是 storeId 的留下)
                       if (ca.categoryId === 0) {
+                        // 點選 全部 類別
                         setShowLikeStores([...likeStores]);
+                        setShowCate("全部店家");
                       } else {
+                        // 點選 其他類別 將其類別濾出後呈現
                         setShowLikeStores(
                           [...likeStores].filter(
                             (v) => Object.values(v)[7] === ca.category
                           )
                         );
+                        setShowCate(ca.category);
                       }
                     }}
                   >
@@ -148,122 +179,140 @@ const UserLike = () => {
         </div>
         <hr></hr>
         {/* -------- 店家收藏清單 開始 -------- */}
-        <div className="text-end fz-md ls-md pt-2">
-          共 {showLikeStores.length} 筆
-        </div>
-        <div className="row">
-          {/* {console.log("aaaaa",likeStores)} */}
-          {/* {likeStores?<></>:(<div>沒有資料</div>)} */}
-          {/* ------- store card 開始 -------- */}
-          {!showLikeStores ? (
-            <div>您還沒有收藏喜愛店家</div>
-          ) : (
-            showLikeStores.map((item) => {
-              return (
-                <div
-                  key={item.storeId}
-                  className="col col-lg-6 col-xl-4 my-4 d-flex justify-content-center flex-wrap"
-                >
-                  <Link to="" className="text-decoration-none card_Link">
-                    <div className="store_Card position-relative">
-                      {/* 卡片的內容 */}
-                      <div className="px-4 pt-4 pb-3 h-100">
-                        {/* 照片 */}
-                        <div className="store_Img mx-auto">
-                          {item.isToday === "營業中" ? (
-                            <span className="position-absolute store_Status_Open badge">
-                              {item.isToday}
-                            </span>
-                          ) : (
-                            <span className="position-absolute store_Status_Close badge">
-                              {item.isToday}
-                            </span>
-                          )}
-                          <img
-                            src={IMAGE_URL + item.storeImg}
-                            className="cover-fit"
-                            alt="storeImage"
-                          />
-                        </div>
-                        {/* 店名 */}
-                        <div className="d-flex align-items-center justify-content-between">
-                          <h4 className="store_Title my-3">
-                            {item.storeName}
-                            <span className="d-block fz-sm mt-2 ls-md">
-                              {item.storeBranchName}
-                            </span>
-                          </h4>
 
-                          <div className="store_Category rounded-pill mb-4">
-                            <span>{item.storeCate}</span>
-                          </div>
-                        </div>
-                        {/* 資訊 時間 剩餘餐點 */}
-                        <div className="d-flex align-items-center justify-content-between store_Info">
-                          <FiClock className="store_Clock me-1" />
-                          <span className="flex-grow-1">
-                            {item.openTime} - {item.closeTime}
-                          </span>
-                          <span className="fz-sm">餐點剩餘:&nbsp;</span>
-                          <span>{item.products}</span>
-                        </div>
-                        {/* 分數相關 */}
-                        <div className="d-flex align-items-center justify-content-between store_Score">
-                          {/* 星星 */}
-                          <div className="d-flex align-items-center">
-                            <Rating
-                              className="store_Star"
-                              // defaultValue={3}
-                              value={Number(item.starScore)}
-                              precision={0.1}
-                              // onChange={handleStar}
-                              readOnly
-                            />
-                            <span className="ls-sm ps-1 fz-md">
-                              {item.starScore} ({item.commentTotal})
-                            </span>
-                          </div>
-                          {/* 愛心 */}
-                          <div
-                            onClick={() => {
-                              // alert(item.storeId);
-                              if (likeStoreIds.includes(item.storeId)) {
-                                // remove
-                                setLikeStoreIds(
-                                  likeStoreIds.filter((v) => v !== item.storeId)
-                                  // 符合條件留下 (比對 不是 storeId 的留下)
-                                );
-                                //setFavAction({ action: 'remove', storeId: id })
-                                handleRemoveLike(item.storeId);
-                              } else {
-                                //add
-                                // setLikeStoreIds([...likeStoreIds, item.storeId]);
-                                //setFavAction({ action: 'add', storeId: id })
-                              }
-                            }}
-                            className="d-flex align-items-center"
-                          >
-                            {likeStoreIds.includes(item.storeId) ? (
-                              <FiHeart className="store_Like_Active" />
-                            ) : (
-                              <FiHeart className="store_Like_unActive" />
-                            )}
+        {loading ? (
+          <BarLoader
+            color={color}
+            size={7}
+            margin={6}
+            loading={loading}
+            css={override}
+            height={6}
+            width={150}
+            speedMultiplier={1}
+          />
+        ) : (
+          <>
+            <div className="text-end fz-md ls-md pt-2">
+              共 {showLikeStores.length} 筆
+            </div>
+            <div className="row">
+              {/* {console.log("aaaaa",likeStores)} */}
+              {/* {likeStores?<></>:(<div>沒有資料</div>)} */}
+              {/* ------- store card 開始 -------- */}
+              {!showLikeStores ? (
+                <div>您還沒有收藏喜愛店家</div>
+              ) : (
+                showLikeStores.map((item) => {
+                  return (
+                    <div
+                      key={item.storeId}
+                      className="col col-lg-6 col-xl-4 my-4 d-flex justify-content-center flex-wrap"
+                    >
+                      <Link to="" className="text-decoration-none card_Link">
+                        <div className="store_Card position-relative">
+                          {/* 卡片的內容 */}
+                          <div className="px-4 pt-4 pb-3 h-100">
+                            {/* 照片 */}
+                            <div className="store_Img mx-auto">
+                              {item.isToday === "營業中" ? (
+                                <span className="position-absolute store_Status_Open badge">
+                                  {item.isToday}
+                                </span>
+                              ) : (
+                                <span className="position-absolute store_Status_Close badge">
+                                  {item.isToday}
+                                </span>
+                              )}
+                              <img
+                                src={IMAGE_URL + item.storeImg}
+                                className="cover-fit"
+                                alt="storeImage"
+                              />
+                            </div>
+                            {/* 店名 */}
+                            <div className="d-flex align-items-center justify-content-between">
+                              <h4 className="store_Title my-3">
+                                {item.storeName}
+                                <span className="d-block fz-sm mt-2 ls-md">
+                                  {item.storeBranchName}
+                                </span>
+                              </h4>
 
-                            <span className="ls-sm ps-1 fz-md">
-                              {item.likeTotal}
-                            </span>
+                              <div className="store_Category rounded-pill mb-4">
+                                <span>{item.storeCate}</span>
+                              </div>
+                            </div>
+                            {/* 資訊 時間 剩餘餐點 */}
+                            <div className="d-flex align-items-center justify-content-between store_Info">
+                              <FiClock className="store_Clock me-1" />
+                              <span className="flex-grow-1">
+                                {item.openTime} - {item.closeTime}
+                              </span>
+                              <span className="fz-sm">餐點剩餘:&nbsp;</span>
+                              <span>{item.products}</span>
+                            </div>
+                            {/* 分數相關 */}
+                            <div className="d-flex align-items-center justify-content-between store_Score">
+                              {/* 星星 */}
+                              <div className="d-flex align-items-center">
+                                <Rating
+                                  className="store_Star"
+                                  // defaultValue={3}
+                                  value={Number(item.starScore)}
+                                  precision={0.1}
+                                  // onChange={handleStar}
+                                  readOnly
+                                />
+                                <span className="ls-sm ps-1 fz-md">
+                                  {item.starScore} ({item.commentTotal})
+                                </span>
+                              </div>
+                              {/* 愛心 */}
+                              <div
+                                onClick={() => {
+                                  // alert(item.storeId);
+                                  if (likeStoreIds.includes(item.storeId)) {
+                                    // remove
+                                    setLikeStoreIds(
+                                      likeStoreIds.filter(
+                                        (v) => v !== item.storeId
+                                      )
+                                      // 符合條件留下 (比對 不是 storeId 的留下)
+                                    );
+                                    //setFavAction({ action: 'remove', storeId: id })
+                                    handleRemoveLike(item.storeId);
+                                  } else {
+                                    //add
+                                    // setLikeStoreIds([...likeStoreIds, item.storeId]);
+                                    //setFavAction({ action: 'add', storeId: id })
+                                  }
+                                }}
+                                className="d-flex align-items-center"
+                              >
+                                {likeStoreIds.includes(item.storeId) ? (
+                                  <FiHeart className="store_Like_Active" />
+                                ) : (
+                                  <FiHeart className="store_Like_unActive" />
+                                )}
+
+                                <span className="ls-sm ps-1 fz-md">
+                                  {item.likeTotal}
+                                </span>
+                              </div>
+                            </div>
                           </div>
                         </div>
-                      </div>
+                      </Link>
                     </div>
-                  </Link>
-                </div>
-              );
-            })
-          )}
-          {/* ------- store card 結束 -------- */}
-        </div>
-        {/* -------- 店家收藏清單 結束 -------- */}
+                  );
+                })
+              )}
+              {/* ------- store card 結束 -------- */}
+            </div>
+            {/* -------- 店家收藏清單 結束 -------- */}
+          </>
+        )}
       </div>
     </>
   );
