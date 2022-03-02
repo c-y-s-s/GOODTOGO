2818075833fd34bcada8fbbfdf60e071bdc61baa
Miru <hungche0701@gmail.com>
2022-03-02 17:34:36 +0800

    feat - 新增商品

--- fe/src/pages/NewProduct/component/Table.js
+++ fe/src/pages/NewProduct/component/Table.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+import axios from "axios";
+import { API_URL } from "../../../utils/config";
+import { ERR_MSG } from "../../../utils/error";
 
 const Table = () => {
+  //預設個欄位的值為空（開發中所以有先給值）
+  const [product, setProduct] = useState({
+    // productName: "奶茶",
+    // productDescription: "測試用商品描述",
+    // amountOfGoods: "10",
+    // commodityPrice: "100",
+    // salesTimeStartmm: "10",
+    // salesTimeStartss: "10",
+    // salesTimeEndmm: "10",
+    // salesTimeEndss: "10",
+    productName: "",
+    productDescription: "",
+    amountOfGoods: "",
+    commodityPrice: "",
+    salesTimeStartmm: "",
+    salesTimeStartss: "",
+    salesTimeEndmm: "",
+    salesTimeEndss: "",
+  });
+  const handleChange = (e) => {
+    setProduct({ ...product, [e.target.name]: e.target.value });
+  };
+  // -------- 表單送出 --------
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      let response = await axios.post(`${API_URL}/storebg/newproduct`, product);
+      console.log(response.data);
+    } catch (e) {
+      // console.error("錯誤:", e.response.data);
+      console.error(ERR_MSG[e.response.data].code);
+    }
+  };
   return (
     <div>
       <form className="container">
@@ -13,7 +52,10 @@ const Table = () => {
               <input
                 className="form-control"
                 id="productName"
+                name="productName"
                 placeholder="請輸入商品名稱"
+                value={product.productName}
+                onChange={handleChange}
                 required
               />
             </div>
@@ -27,8 +69,11 @@ const Table = () => {
               <textarea
                 className="form-control"
                 id="productDescription"
+                name="productDescription"
                 rows="3"
                 placeholder="請輸入商品描述"
+                value={product.productDescription}
+                onChange={handleChange}
                 required
               ></textarea>
             </div>
@@ -39,7 +84,10 @@ const Table = () => {
               <input
                 className="form-control"
                 id="amountOfGoods"
+                name="amountOfGoods"
                 placeholder="請輸入商品數量"
+                value={product.amountOfGoods}
+                onChange={handleChange}
                 required
               />
             </div>
@@ -58,31 +106,62 @@ const Table = () => {
                   type="text"
                   className="form-control"
                   id="commodityPrice"
+                  name="commodityPrice"
                   aria-describedby="inputGroupPrepend"
                   placeholder="請輸入商品價格"
+                  value={product.commodityPrice}
+                  onChange={handleChange}
                   required
                 />
                 <div className="invalid-feedback">請輸入商品價格.</div>
               </div>
             </div>
             <div className="row align-items-center">
-              <label htmlFor="SalesTime" className="form-label fw-bold">
+              <label htmlFor="salesTime" className="form-label fw-bold">
                 販售時間
               </label>
               <div className="col">
-                <input className="form-control" id="SalesTime" required />
+                <input
+                  className="form-control"
+                  id="salesTimeStartmm"
+                  name="salesTimeStartmm"
+                  value={product.salesTimeStartmm}
+                  onChange={handleChange}
+                  required
+                />
               </div>
               ：
               <div className="col">
-                <input className="form-control" id="SalesTime" required />
+                <input
+                  className="form-control"
+                  id="salesTimeStartss"
+                  name="salesTimeStartss"
+                  value={product.salesTimeStartss}
+                  onChange={handleChange}
+                  required
+                />
               </div>
               ～
               <div className="col">
-                <input className="form-control" id="SalesTime" required />
+                <input
+                  className="form-control"
+                  id="salesTimeEndmm"
+                  name="salesTimeEndmm"
+                  value={product.salesTimeEndmm}
+                  onChange={handleChange}
+                  required
+                />
               </div>
               ：
               <div className="col">
-                <input className="form-control" id="SalesTime" required />
+                <input
+                  className="form-control"
+                  id="salesTimeEndss"
+                  name="salesTimeEndss"
+                  value={product.salesTimeEndss}
+                  onChange={handleChange}
+                  required
+                />
               </div>
             </div>
           </div>
@@ -112,12 +191,18 @@ const Table = () => {
         </div>
         <div className="row  mt-3">
           <div className="col-3"></div>
-          <button type="button" className="col-2 btn btn-lg cancel-bg">
+
+          <NavLink type="button" className="col-2 btn btn-lg cancel-bg" to="/storebg">
             取消
-          </button>
+          </NavLink>
+
           <div className="col-1"></div>
           <div className="col-1"></div>
-          <button type="submit" className="col-2 btn btn-warning btn-lg ">
+          <button
+            type="submit"
+            className="col-2 btn btn-warning btn-lg "
+            onClick={handleSubmit}
+          >
             上架
           </button>
         </div>
