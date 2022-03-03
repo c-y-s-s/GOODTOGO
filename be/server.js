b17d8c815c8a5cb398adb8321542bcd49be2c726
Miru <hungche0701@gmail.com>
2022-03-03 16:32:56 +0800

    feat - 增加 商家後台寫入資料

--- be/server.js
+++ be/server.js
@@ -69,6 +69,11 @@ app.use("/api/member", memberRouter);
 let storebgRouter = require("./routers/storebg");
 app.use("/api/storebg", storebgRouter);
 // -------- 商家後台 RESTful API 列表 --------
+
+// -------- 商家後台寫入資料 --------
+let storeBgAddProduct = require("./routers/storeBgAddProduct");
+app.use("/api/storebgaddproduct", storeBgAddProduct);
+
 // 404
 app.use((req, res, next) => {
   res.status(404).send("404");
