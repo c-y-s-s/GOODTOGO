dd921e463f69a6607cc421ff625774ef48b61249
Your hadeli <ch96963@gmail.com>
2022-03-04 12:55:25 +0800

    docs - 更改檔名

--- fe/src/pages/Product/index.js
+++ fe/src/pages/Product/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import { API_URL } from "../../utils/config";
 
