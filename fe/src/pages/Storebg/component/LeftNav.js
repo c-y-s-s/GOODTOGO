1c1151b99fa50fbdb7cd572a5b1f8c1b033050f1
Miru <hungche0701@gmail.com>
2022-03-08 06:58:02 +0800

    feat - 刪除多於程式碼

--- fe/src/pages/Storebg/component/LeftNav.js
+++ fe/src/pages/Storebg/component/LeftNav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ReactComponent as Logo } from "../../../images/logo.svg";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import icon1 from "../images/Vector3.png";
 
 const DataLeftNav = () => {
@@ -10,20 +10,15 @@ const DataLeftNav = () => {
         {/* -------- 左邊欄位區塊開始 --------*/}
         <div className="background-storebg-data-left-nav sticky-top">
           <div className="background-storebg-data-left-nav-logo">
-            <Logo />
+            <Link to="/">
+              <Logo />
+            </Link>
           </div>
           <ul>
-            {/* <li className="">
-              <a href="#/">
-                <img src={icon1} style={icon1.logo} alt="fireSpot" />
-                商家資料編輯
-              </a>
-            </li> */}
-
             <li>
               <NavLink
                 className={({ isActive }) => (isActive ? "ms-3" : "")}
-                to="/storebg"
+                to="/"
               >
                 <img
                   src={icon1}
@@ -48,20 +43,6 @@ const DataLeftNav = () => {
                 商品新增
               </NavLink>
             </li>
-            {/* <li>
-              <NavLink
-                className={({ isActive }) => (isActive ? "ms-3" : "")}
-                to="/productedit"
-              >
-                <img
-                  src={icon1}
-                  className="ms-2"
-                  style={icon1.logo}
-                  alt="fireSpot"
-                />
-                商品資料編輯
-              </NavLink>
-            </li> */}
           </ul>
         </div>
         {/* -------- 左邊欄位區塊結束 --------*/}
