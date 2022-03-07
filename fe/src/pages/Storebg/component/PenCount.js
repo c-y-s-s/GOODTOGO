eec73599afe8f970ccfa83e14bbb2b94a808b388
Miru <hungche0701@gmail.com>
2022-03-08 06:58:23 +0800

    fix - 修改合併後無法出現筆數問題

--- fe/src/pages/Storebg/component/PenCount.js
+++ fe/src/pages/Storebg/component/PenCount.js
@@ -1,17 +1,23 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useAuth } from "../../../context/auth";
+
 
 import { API_URL } from "../../../utils/config";
 
 const PenCount = () => {
+  const { loginSeller } = useAuth();
+
   // 店家商品列表
   const [productsData, setproductsData] = useState([]);
 
   useEffect(() => {
+    // console.log(loginSeller,"111111")
     // http://localhost:3002/api/member/proile
     let setProducts = async () => {
-      let response = await axios.get(`${API_URL}/storebg/productslist`);
+      let response = await axios.get(`${API_URL}/storebg/productslist?store_id=${loginSeller.id}`);
       let productsList = response.data[0];
+      // console.log("10000000000000000000",productsList)
       setproductsData(productsList);
     };
     setProducts();
