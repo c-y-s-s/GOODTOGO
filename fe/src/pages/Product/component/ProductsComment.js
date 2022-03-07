25d2f23de536118ba9fa8fcafb3b4556b3a15070
Chang <ch96963@gmail.com>
2022-03-08 01:26:05 +0800

    style - 增加商品評論 loading 樣式

--- fe/src/pages/Product/component/ProductsComment.js
+++ fe/src/pages/Product/component/ProductsComment.js
@@ -15,9 +15,10 @@ import { API_URL } from "../../../utils/config";
 import { v4 as uuidv4 } from "uuid";
 // -------- 商品評論 --------
 const StoreProductsComment = () => {
+
   const { storeId } = useParams();
   const { currentPage } = useParams();
-
+  const [isLoading, setIsLoading] = useState(true);
   // 存指定商家 ID 評論
   const [productsComment, setproductsComment] = useState([]);
   // 存總筆數
@@ -34,6 +35,7 @@ const StoreProductsComment = () => {
 
   // 撈指定商家評論
   useEffect(() => {
+        setIsLoading(true);
     let getComment = async () => {
       // 預設 create_time DESC API
       let productsCommentResponse = await axios.get(
@@ -69,6 +71,9 @@ const StoreProductsComment = () => {
       }
     };
     getComment();
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 100);
   }, [page, productsCommitTimeSortSwitch, productsCommitStarSortSwitch]);
 
   // productsComment.map((item) => {
@@ -106,166 +111,188 @@ const StoreProductsComment = () => {
     );
   }
 
+    const spinner = (
+      <>
+        {/* <div className="spinner-grow text-primary" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div> */}
+        <div className="text-center loading-img mt-5">
+          {/* <div className="xxxx">1234</div> */}
+          <img
+            className=""
+            src={require(`../images/editor-2.7s-40px.gif`)}
+            alt=""
+          />
+       
+        </div>
+      </>
+    );
   return (
     <div>
-      <div className="container products-comment">
-        <div>
-          <div className="d-flex  justify-content-between text-end products-comment-total">
-            <div className="d-flex product-users-comment-filter">
-              <div className="me-2 product-users-comment-filter-star">
-                評分
-                <button
-                  className="product-users-comment-filter-icon"
-                  onClick={handleStarSort}
-                >
-                  <RiArrowUpDownFill />
-                </button>
+      {isLoading ? (
+        spinner
+      ) : (
+        <div className="container products-comment">
+          <div>
+            <div className="d-flex  justify-content-between text-end products-comment-total">
+              <div className="d-flex product-users-comment-filter">
+                <div className="me-2 product-users-comment-filter-star">
+                  評分
+                  <button
+                    className="product-users-comment-filter-icon"
+                    onClick={handleStarSort}
+                  >
+                    <RiArrowUpDownFill />
+                  </button>
+                </div>
+                <div className="product-users-comment-filter-time">
+                  留言時間
+                  <button
+                    className="product-users-comment-filter-icon"
+                    onClick={handleTimeSort}
+                  >
+                    <RiArrowUpDownFill />
+                  </button>
+                </div>
               </div>
-              <div className="product-users-comment-filter-time">
-                留言時間
-                <button
-                  className="product-users-comment-filter-icon"
-                  onClick={handleTimeSort}
-                >
-                  <RiArrowUpDownFill />
-                </button>
-              </div>
+              <div>共 {totalPages} 則留言</div>
             </div>
-            <div>共 {totalPages} 則留言</div>
           </div>
-        </div>
-        {productsComment.map((item) => {
-          return (
-            <div className="col-12 mt-3 product-comment" key={uuidv4()}>
-              <div className="d-flex justify-content-between ">
-                <div className="d-flex user-data w-100">
-                  <div>
-                    <div className="user-photo mt-3">
-                      <img
-                        className="cover-photo"
-                        src={require(`../../../images/store_img/01.jpg`)}
-                        alt=""
-                      />
-                    </div>
-                  </div>
-
-                  <div className="ms-4 w-100">
-                    <div className="d-flex justify-content-between align-items-center flex-wrap">
-                      <div className="pt-2 user-data-name">{item.name}</div>
-                      <div className="d-flex align-items-center">
-                        <div className="user-data-comment-time">
-                          {item.create_time}
-                        </div>
-                        <div className="">
-                          <button
-                            className=" user-data-report"
-                            type="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                          >
-                            <FiMoreVertical />
-                          </button>
-                          <ul className="dropdown-menu">
-                            <a href="">檢舉</a>
-                          </ul>
-                        </div>
+          {productsComment.map((item) => {
+            return (
+              <div className="col-12 mt-3 product-comment" key={uuidv4()}>
+                <div className="d-flex justify-content-between ">
+                  <div className="d-flex user-data w-100">
+                    <div>
+                      <div className="user-photo mt-3">
+                        <img
+                          className="cover-photo"
+                          src={require(`../../../images/store_img/01.jpg`)}
+                          alt=""
+                        />
                       </div>
                     </div>
-                    <div className="d-flex justify-content-between  product-data">
-                      <div>
-                        <div className="d-flex ">
+
+                    <div className="ms-4 w-100">
+                      <div className="d-flex justify-content-between align-items-center flex-wrap">
+                        <div className="pt-2 user-data-name">{item.name}</div>
+                        <div className="d-flex align-items-center">
+                          <div className="user-data-comment-time">
+                            {item.create_time}
+                          </div>
                           <div className="">
-                            <div className="d-flex ">
-                              <Stack spacing={1}>
-                                <Rating
-                                  name="half-rating-read"
-                                  defaultValue={item.star}
-                                  precision={0.1}
-                                  readOnly
-                                />
-                              </Stack>
-                              <div className="ps-2 product-data-name">
-                                {item.products_name}
+                            <button
+                              className=" user-data-report"
+                              type="button"
+                              data-bs-toggle="dropdown"
+                              aria-expanded="false"
+                            >
+                              <FiMoreVertical />
+                            </button>
+                            <ul className="dropdown-menu">
+                              <a href="">檢舉</a>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="d-flex justify-content-between  product-data">
+                        <div>
+                          <div className="d-flex ">
+                            <div className="">
+                              <div className="d-flex ">
+                                <Stack spacing={1}>
+                                  <Rating
+                                    name="half-rating-read"
+                                    defaultValue={item.star}
+                                    precision={0.1}
+                                    readOnly
+                                  />
+                                </Stack>
+                                <div className="ps-2 product-data-name">
+                                  {item.products_name}
+                                </div>
                               </div>
                             </div>
+                            <div className="ps-3 product-data-name">
+                              {item.products}
+                            </div>
                           </div>
-                          <div className="ps-3 product-data-name">
-                            {item.products}
-                          </div>
+                          <p className="pt-2 mb-0 user-comment">
+                            {item.comment}
+                          </p>
                         </div>
-                        <p className="pt-2 mb-0 user-comment">{item.comment}</p>
+                        <img
+                          className="product-photo "
+                          src={require(`../../../images/products_img/${item.img}`)}
+                          alt=""
+                        />
                       </div>
-                      <img
-                        className="product-photo "
-                        src={require(`../../../images/products_img/${item.img}`)}
-                        alt=""
-                      />
+                    </div>
+                  </div>
+                  <div className="text-end">
+                    <div className="d-flex">
+                      <div className="d-flex"></div>
                     </div>
                   </div>
                 </div>
-                <div className="text-end">
-                  <div className="d-flex">
-                    <div className="d-flex"></div>
-                  </div>
-                </div>
               </div>
+            );
+          })}
+          {/* // 頁碼功能 */}
+          {/* // ! Link尚未設定錨點 */}
+          <div className="products-comment-pagination">
+            <div className="pages-icon">
+              {page <= 1 ? (
+                <Link
+                  to={`${page}#mark-1`}
+                  className="comment-page-arrow page-opcity"
+                  onClick={() => {
+                    setPage(page);
+                  }}
+                >
+                  <FiChevronLeft />
+                </Link>
+              ) : (
+                <Link
+                  to={`${page - 1}#mark-1`}
+                  className="comment-page-arrow"
+                  onClick={() => {
+                    setPage(page - 1);
+                  }}
+                >
+                  <FiChevronLeft />
+                </Link>
+              )}
             </div>
-          );
-        })}
-        {/* // 頁碼功能 */}
-        {/* // ! Link尚未設定錨點 */}
-        <div className="products-comment-pagination">
-          <div className="pages-icon">
-            {page <= 1 ? (
-              <Link
-                to={`${page}#mark-1`}
-                className="comment-page-arrow page-opcity"
-                onClick={() => {
-                  setPage(page);
-                }}
-              >
-                <FiChevronLeft />
-              </Link>
-            ) : (
-              <Link
-                to={`${page - 1}#mark-1`}
-                className="comment-page-arrow"
-                onClick={() => {
-                  setPage(page - 1);
-                }}
-              >
-                <FiChevronLeft />
-              </Link>
-            )}
-          </div>
-          <div>
-            {pages.map((item) => {
-              return item;
-            })}
-          </div>
-          <div className="pages-icon">
-            {page >= lastPage ? (
-              <Link
-                to={`${page}`}
-                className="comment-page-arrow page-opcity"
-                onClick={() => {
-                  setPage(page);
-                }}
-              ></Link>
-            ) : (
-              <Link
-                to={`${page + 1}`}
-                className="comment-page-arrow"
-                onClick={() => {
-                  setPage(page + 1);
-                }}
-              >
-                <FiChevronRight />
-              </Link>
-            )}
+            <div>
+              {pages.map((item) => {
+                return item;
+              })}
+            </div>
+            <div className="pages-icon">
+              {page >= lastPage ? (
+                <Link
+                  to={`${page}`}
+                  className="comment-page-arrow page-opcity"
+                  onClick={() => {
+                    setPage(page);
+                  }}
+                ></Link>
+              ) : (
+                <Link
+                  to={`${page + 1}`}
+                  className="comment-page-arrow"
+                  onClick={() => {
+                    setPage(page + 1);
+                  }}
+                >
+                  <FiChevronRight />
+                </Link>
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
