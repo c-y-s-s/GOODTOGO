b3999cfa9a53ec53080052a41c0a471e848176e9
Infiltrators-Elixir <bhst1124@gmail.com>
2022-03-08 23:05:39 +0800

    fix - agreement checlbox location and storeType input box err message repaired

--- fe/src/pages/StoreCheck/index.js
+++ fe/src/pages/StoreCheck/index.js
@@ -630,7 +630,7 @@ console.log(closeDay)
                           </select>
                         </div> */}
                         <div className="col-6">
-                          <label>City</label>
+                          <label>縣市</label>
                           <select className={`form-control custom-input ${fieldErrors.City !== "" && "input-error"
                             }`}
                             name="City"
@@ -655,7 +655,7 @@ console.log(closeDay)
                         </div>
 
                         <div className="col-6">
-                          <label>選擇區域</label>
+                          <label>區域</label>
                           <select className={`form-control custom-input ${fieldErrors.Area !== "" && "input-error"
                             }`}
                             name="Area"
@@ -851,7 +851,8 @@ console.log(closeDay)
                       <div className="form-floating mb-3">
                         <select
                           name="storeType"
-                          className="form-control custom-input"
+                          className={`form-control custom-input ${fieldErrors.storeType !== "" && "input-error"
+                        }`}
                           id="storeType"
                           placeholder="商品類別"
                           value={member.storeType}
@@ -874,9 +875,11 @@ console.log(closeDay)
                         >
                           主打的是...
                         </label>
-                      </div>
-
-
+                        {fieldErrors.storeType !== "" && (
+                          <div className="error text-end">
+                            {fieldErrors.storeType}
+                          </div>
+                        )}</div>
                       {/* ------- 營業星期 (複選) -------- */}
                       <label
                         htmlFor="opendayCheck"
@@ -1048,7 +1051,7 @@ console.log(closeDay)
                       </div>
                       {/* -------- 營業時間設定結束 -------- */}
                       {/* -------- 註冊同意按鈕開始 -------- */}
-                      <div className="align-items-center text-grey input-label-title">
+                      <div className="align-items-center text-grey input-label-title mt-5">
                         <input
                           type="checkbox"
                           checked={agree}
