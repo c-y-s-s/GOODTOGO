8fc9dc9e7a752f9f7ae1e887c8a17c2e05e0d9bc
ECHOSHIH <chunyushih@outlook.com>
2022-03-08 16:24:55 +0800

    feat- 地圖可以抓到使用者當前位置

--- fe/src/pages/Map/components/CurrentLocationPin.js
+++ fe/src/pages/Map/components/CurrentLocationPin.js
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
-import { MdLocationPin } from "react-icons/md";
+import { MdLocationPin, MdOutlineEmojiPeople } from "react-icons/md";
 import { ReactComponent as Pin } from "../images/pin_map.svg";
 
 const LocationPin = (props) => {
   // console.log("LocationPin裡面", props);
   return (
     <div className="pin d-flex flex-column align-items-center">
-      <p className="pin-text d-flex flex-column align-items-center">
+      {/* <p className="pin-text d-flex flex-column align-items-center">
         <div className="title pt-2">{props.name}</div>
         <div className="time pt-2">
           {props.open_time} - {props.close_time}
         </div>
         <div className="product-left mt-2">剩餘: {props.amount}</div>
-      </p>
+      </p> */}
 
-      <MdLocationPin
+      <MdOutlineEmojiPeople
         role="button"
         className="pin-icon"
-        onClick={() => {
-          props.setClickStoreId(props.storeId);
-        }}
+        // onClick={() => {
+        //   props.setClickStoreId(props.storeId);
+        // }}
       />
     </div>
   );
--- fe/src/pages/Map/components/Map.js
+++ fe/src/pages/Map/components/Map.js
@@ -1,14 +1,25 @@
 import GoogleMapReact from "google-map-react";
-import React from "react";
+import React, { useState } from "react";
 import LocationPin from "./LocationPin";
+import CurrentLocationPin from "./CurrentLocationPin";
+import { MdOutlineEmojiPeople } from "react-icons/md";
 
 const Map = (props) => {
   const { displayStoreList, zoomLevel } = props;
+  const [currentLat, setCurrentLat] = useState("");
+  const [currentLng, setCurrentLng] = useState("");
   //!渲染是靠location
   //!LocationPin 必需要有相對應的location才能渲染出東西
   //!也就是說必需先整理出需要的location 陣列
   //!然後利用去map這個陣列然後 裡面map LocationPin
 
+  const here = navigator.geolocation.getCurrentPosition(function (position) {
+    console.log(position);
+    setCurrentLat(position.coords.latitude);
+    setCurrentLng(position.coords.longitude);
+  });
+
+  console.log("clng", currentLng, "clat", currentLat);
   // 先把得到的資料塞進一個空陣列裡
   const locationArr = [];
   displayStoreList.forEach((i) => {
@@ -25,14 +36,16 @@ const Map = (props) => {
   });
   console.log("sss", displayStoreList);
   const location = {
-    address: "320桃園市中壢區新生路二段421號",
-    lat: 24.985128,
-    lng: 121.221719,
+    // address: "320桃園市中壢區新生路二段421號",
+    lat: currentLat,
+    lng: currentLng,
+    // name: "你在這",
+    // lat: 25.033964,
+    // lng: 121.564468,
   };
   // const location2 = {
   //   address: "101",
-  //   lat: 25.033964,
-  //   lng: 121.564468,
+  //
   // };
   // console.log(displayStoreList, "lsit");
   return (
@@ -50,10 +63,10 @@ const Map = (props) => {
           >
             {/* {storeLIst.map(()=>{return()})} */}
             {/* 使用者位置，預設學校 */}
-            <LocationPin
+            <CurrentLocationPin
               className="location-pin"
-              lat={location.lat}
-              lng={location.lng}
+              lat={currentLat}
+              lng={currentLng}
               name={location.name}
             />
 
