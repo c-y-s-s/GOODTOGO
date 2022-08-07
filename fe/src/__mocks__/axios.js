ad4394badd9480c9ee849fe3e57961943a448b3e
LAPTOP-3HT9DOHB\Chang <ch96963@gmail.com>
2022-08-07 16:44:38 +0800

    Update Mock Axios

--- fe/src/__mocks__/axios.js
+++ fe/src/__mocks__/axios.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    get:jest.fn().mockResolvedValue(
-    {data:{}}
-    )
-}
+  post: jest.fn().mockResolvedValue({ data: {} }),
+  get: jest.fn().mockResolvedValue({ data: {} }),
+};
+
